957d2400ebad398ceaeb1c6032f7678d2fbfc9a8
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2021-11-30 16:52:15 -0300

    objeto java users final

---
+++ src/main/java/br/com/livrobeckend/userapi/model/Users.java
@@ -0,0 +1,96 @@
+package br.com.livrobeckend.userapi.model;
+
+import br.com.livrobeckend.userapi.dto.UserDTO;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+
+@Entity
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private String email;
+    private String telefone;
+    private Date dataCadastro;
+
+
+    //Getters e setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    //Metodo
+    public static Users convert(UserDTO userDTO) {
+        // Objeto criado tem quue ter o mesmo
+        // nome da tabela do banco de dados
+        Users users = new Users();
+        users.setNome(userDTO.getNome());
+        users.setEndereco(userDTO.getEndereco());
+        users.setCpf(userDTO.getCpf());
+        users.setEmail(userDTO.getEmail());
+        users.setTelefone(userDTO.getTelefone());
+        users.setDataCadastro(userDTO.getDataCadastro());
+        return users;
+    }
+}
+
