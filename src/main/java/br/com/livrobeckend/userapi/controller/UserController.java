02dab8ab02deb17992623b3011cd6a4a4e36c015
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2021-11-30 09:40:50 -0300

    todos metodos de controller

--- src/main/java/br/com/livrobeckend/userapi/controller/UserController.java
+++ src/main/java/br/com/livrobeckend/userapi/controller/UserController.java
@@ -1,7 +1,12 @@
 package br.com.livrobeckend.userapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.livrobeckend.userapi.dto.UserDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+import static br.com.livrobeckend.userapi.dto.UserDTO.usuarios;
 
 @RestController
 public class UserController {
@@ -9,4 +14,38 @@ public class UserController {
     public String getMensagem(){
         return "Spring boot esta funcionando";
     }
+
+    @GetMapping("/users")
+    public List<UserDTO> getUsers() {
+        return usuarios;
+    }
+
+    @GetMapping("/users/{cpf}")
+    public UserDTO getUsersFiltro(@PathVariable String cpf) {
+        for (UserDTO userFiler: usuarios) {
+            if (userFiler.getCpf().equals(cpf)) {
+                return userFiler;
+            }
+        }
+        return null;
+    }
+
+    @PostMapping("/newUser")
+    public UserDTO inserir(@RequestBody UserDTO userDTO){
+        userDTO.setDataCadastro(new Date());
+        usuarios.add(userDTO);
+        return userDTO;
+    }
+
+    @DeleteMapping("user/{cpf}")
+    public boolean remover(@PathVariable String cpf) {
+        for (UserDTO userFilter: usuarios) {
+            if (userFilter.getCpf().equals(cpf)){
+                usuarios.remove(userFilter);
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
