f90718add68a9edad44d8dd4d1fa6097fcba4256
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2021-11-30 16:53:23 -0300

    camada controller final

--- src/main/java/br/com/livrobeckend/userapi/controller/UserController.java
+++ src/main/java/br/com/livrobeckend/userapi/controller/UserController.java
@@ -1,51 +1,53 @@
 package br.com.livrobeckend.userapi.controller;
 
 import br.com.livrobeckend.userapi.dto.UserDTO;
+import br.com.livrobeckend.userapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 import java.util.List;
 
-import static br.com.livrobeckend.userapi.dto.UserDTO.usuarios;
 
 @RestController
 public class UserController {
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/")
     public String getMensagem(){
         return "Spring boot esta funcionando";
     }
 
-    @GetMapping("/users")
+    @GetMapping("/user/")
     public List<UserDTO> getUsers() {
+        List<UserDTO> usuarios = userService.getAll();
         return usuarios;
     }
 
-    @GetMapping("/users/{cpf}")
-    public UserDTO getUsersFiltro(@PathVariable String cpf) {
-        for (UserDTO userFiler: usuarios) {
-            if (userFiler.getCpf().equals(cpf)) {
-                return userFiler;
-            }
-        }
-        return null;
+    @GetMapping("/user/{id}")
+    UserDTO findById(@PathVariable Long id) {
+        return userService.findById(id);
     }
 
-    @PostMapping("/newUser")
-    public UserDTO inserir(@RequestBody UserDTO userDTO){
-        userDTO.setDataCadastro(new Date());
-        usuarios.add(userDTO);
-        return userDTO;
+    @PostMapping("/user")
+    UserDTO newUser(@RequestBody UserDTO userDTO) {
+        return userService.save(userDTO);
     }
 
-    @DeleteMapping("user/{cpf}")
-    public boolean remover(@PathVariable String cpf) {
-        for (UserDTO userFilter: usuarios) {
-            if (userFilter.getCpf().equals(cpf)){
-                usuarios.remove(userFilter);
-                return true;
-            }
-        }
-        return false;
+    @GetMapping("/user/cpf/{cpf}")
+    UserDTO findByCpf(@PathVariable String cpf) {
+        return userService.findByCpf(cpf);
     }
 
+    @DeleteMapping("/user/{id}")
+    UserDTO delete(@PathVariable Long id) {
+        return userService.delete(id);
+    }
+
+    @GetMapping("/user/search")
+    public List<UserDTO> queryByName(
+            @RequestParam(name="nome", required = true)
+                    String nome) {
+        return userService.queryByName(nome);
+    }
 }
