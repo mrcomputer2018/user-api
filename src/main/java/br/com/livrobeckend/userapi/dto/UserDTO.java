f625d76cd5b78685ec384b3983a7034bd944d0f1
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2021-11-30 09:00:32 -0300

    Lista de usuarios

--- src/main/java/br/com/livrobeckend/userapi/dto/UserDTO.java
+++ src/main/java/br/com/livrobeckend/userapi/dto/UserDTO.java
@@ -1,6 +1,9 @@
 package br.com.livrobeckend.userapi.dto;
 
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class UserDTO {
     // Atributos
@@ -10,6 +13,9 @@ public class UserDTO {
     private String email;
     private String telefone;
     private Date dataCadastro;
+
+    //Construtor
+    public static List<UserDTO> usuarios = new ArrayList<UserDTO>();
 
     //Getters e setters
     public String getNome() {
@@ -47,5 +53,37 @@ public class UserDTO {
     }
     public void setDataCadastro(Date dataCadastro) {
         this.dataCadastro = dataCadastro;
+    }
+
+    //Metodos
+    @PostConstruct
+    public void initiateList() {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setNome("Eduardo");
+        userDTO.setCpf("123");
+        userDTO.setEndereco("Rua a");
+        userDTO.setEmail("eduardo@email.com");
+        userDTO.setTelefone("1234-3454");
+        userDTO.setDataCadastro(new Date());
+
+        UserDTO userDTO2 = new UserDTO();
+        userDTO2.setNome("Luiz");
+        userDTO2.setCpf("456");
+        userDTO2.setEndereco("Rua b");
+        userDTO2.setEmail("luiz@email.com");
+        userDTO2.setTelefone("1234-3454");
+        userDTO2.setDataCadastro(new Date());
+
+        UserDTO userDTO3 = new UserDTO();
+        userDTO3.setNome("Bruna");
+        userDTO3.setCpf("678");
+        userDTO3.setEndereco("Rua c");
+        userDTO3.setEmail("bruna@email.com");
+        userDTO3.setTelefone("1234-3454");
+        userDTO3.setDataCadastro(new Date());
+
+        usuarios.add(userDTO);
+        usuarios.add(userDTO2);
+        usuarios.add(userDTO3);
     }
 }
