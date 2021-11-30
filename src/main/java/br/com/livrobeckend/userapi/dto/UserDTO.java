fdec29796383ef2f1ac6161ab9fe7d38cece395a
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2021-11-30 08:19:24 -0300

    objeto java inicial

---
+++ src/main/java/br/com/livrobeckend/userapi/dto/UserDTO.java
@@ -0,0 +1,51 @@
+package br.com.livrobeckend.userapi.dto;
+
+import java.util.Date;
+
+public class UserDTO {
+    // Atributos
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private String email;
+    private String telefone;
+    private Date dataCadastro;
+
+    //Getters e setters
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+}
